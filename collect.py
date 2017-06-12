85d2ade7e6ede4adfc61827d88037b9679921384
Emanuele Dalla Longa <3xplosive.g@gmail.com>
2017-06-12 21:00:34 +0200

    Fixed collect.py

--- collect.py
+++ collect.py
@@ -12,23 +12,27 @@ d1= -40
 d2=  0.01
 
 ser=serial.Serial('/dev/ttyUSB0', 115200, timeout=5)
-ser.flushInput()
-time.sleep(0.1)
-starttime=time.time()
-while(time.time()-starttime<1):
-	ser.readline()
 
+starttime=time.time()
+while(time.time()-starttime<2):
+	ser.read(21)
+
+time.sleep(.5)
 data={}
+
 while(True):
-	data_raw = ser.readline()
-	if data_raw==b'': continue
+	data_raw = ser.read(21)
+	if data_raw==b'': 
+		continue
 	try:
 		data_array=struct.unpack_from('>IIIHH',data_raw,2)
 	except:
 		print("Failed to parse, continuing...", file=sys.stderr)
+		print("Data was: "+str(data_raw),file=sys.stderr)
 		continue
 	data["counter"] = data_array[2]
 	data["temperature"] = d1 + d2*data_array[3]
 	data["humidity"] = c1 + c2*data_array[4] + c3*pow(data_array[4],2)
+	print("Raw data: "+str(data_raw),file=sys.stderr)
 	print('{{ "id": {}, "temperature": {}, "humidity": {} }}'
 		.format(data["counter"],round(data["temperature"],2),round(data["humidity"],2)))
