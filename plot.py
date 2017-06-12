3f68483f73a095d84a8058fdf670405ba571c8f6
Emanuele Dalla Longa <3xplosive.g@gmail.com>
2017-06-12 22:24:40 +0200

    Resetting

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import sys
 with open(sys.argv[1],'r') as input:
     acq=list(csv.reader(input))
 
-acq=[ [abs(int(row[0])),abs(float(row[1])),abs(float(row[2]))] for row in acq if (float(row[0])<50000 and float(row[1])<40 and float(row[2])<70 and float(row[2])>5) ] 
+acq=[ [int(row[0]),float(row[1]),float(row[2])] for row in acq] 
 
 timeaxis=[x[0] for x in acq]
 temp=[x[1] for x in acq]
@@ -16,4 +16,3 @@ import matplotlib.pyplot as p
 
 p.plot(timeaxis, temp, 'r', timeaxis, humid, 'b')
 p.savefig(sys.argv[1]+".png")
-
