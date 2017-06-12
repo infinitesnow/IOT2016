c5378fc3c07026a5aa2964f47afcad75bf0c4792
Emanuele Dalla Longa <3xplosive.g@gmail.com>
2017-06-13 00:36:14 +0200

    Changed frequency and invalid sample value

--- shared/Sampler.h
+++ shared/Sampler.h
@@ -1,10 +1,10 @@
 #ifndef SAMPLER_H
 #define SAMPLER_H
 
-#define INVALID_SAMPLE_VALUE 0xFFFF
+#define INVALID_SAMPLE_VALUE 0x0000
 #define UQ_SAMPLER "Sampler"
 
-uint32_t SAMPLE_INTERVAL = 10*1024U; // How often to sample sensors
+uint32_t SAMPLE_INTERVAL = 5*1024U; // How often to sample sensors
 
 typedef nx_struct Entry {
   nx_uint32_t counter;
